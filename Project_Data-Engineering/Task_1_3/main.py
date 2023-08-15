a0da1c250725a2e6a8ae27f51618ffcf06819a88
Oxana Lezhneva <Oxlezhneva@yandex.ru>
2023-08-15 12:31:57 +0300

    first commit

--- Project_Data-Engineering/Task_1_3/main.py
+++ Project_Data-Engineering/Task_1_3/main.py
@@ -5,146 +5,148 @@ from os import getenv
 from dotenv import load_dotenv, find_dotenv #(python-dotenv)
 import sys
 from pathlib import Path
+import datetime
+load_dotenv(find_dotenv()) # Для использования переменных окружения.
 
-# Для использования переменных окружения.
-load_dotenv(find_dotenv())
 
-# Переменные окружения. Создайте в корне папки файл .env и укажите значения переменных для 
-# подключения к базе PostgreSQL (шаблон в env _example.txt). Можно заполнить вручную, например "DB_NAME = postgres".
+def connect_postgreSQL():
+    """
+    Провека подключения к PosgreSQL.
+    """
+    #Переменные окружения
+    DB_NAME = getenv("DB_NAME")
+    DB_HOST=getenv("DB_HOST")
+    DB_PORT=getenv("DB_PORT")
+    DB_USER=getenv("DB_USER")
+    DB_PASSWORD=getenv("DB_PASSWORD")
+    engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    try:
+        engine.connect()
+        print("\nПодключение к PostgreSQL успешно.\n") 
+        return engine  
+    except:        
+        print(f'\nОШИБКА соединения c PostgreSQL: \n {sys.exc_info()}\n')
 
-DB_NAME = getenv("DB_NAME")
-DB_HOST=getenv("DB_HOST")
-DB_PORT=getenv("DB_PORT")
-DB_USER=getenv("DB_USER")
-DB_PASSWORD=getenv("DB_PASSWORD")
- 
-# Название таблиц и схемы в PostgreSQL.
-schema = 'dm'
-table_name = 'dm_f101_round_f'
-copy_table_name = 'dm_f101_round_f_2'
 
-# Путь к файлу .csv
-file_to_open = Path("Project_Data-Engineering/Task_1_3/data")/ f"{table_name}.csv" 
-  
-def logging(status, engine, description='', error=''):
-
-    """ Загрузка данных в таблицу логов. """   
-    with engine.connect() as conn:
-        #Создание таблицы логов         
+def сreate_table_logging(engine):    
+    """
+    Создание таблицы логов.
+    """
+    with engine.connect() as conn:           
         conn.execute(text(f"""create table if not exists logs.extract_load_logs(
                                 action_date timestamp not null default now(),
                                 status varchar(40),
                                 description text,
-                                error text);"""))          
+                                error text);"""))         
+        conn.commit() 
+        conn.close() 
+
+
+def logging(status, engine, description='', error=''):
+    """
+    Загрузка данных в таблицу логов.
+    """   
+    with engine.connect() as conn:              
         conn.execute(text(f"""insert into logs.extract_load_logs (status, description, error)
                           values ('{status}', '{description}', '{error}');"""))        
-        conn.commit()         
+        conn.commit()
+        conn.close()          
 
 def exist_table(engine, schema, table_name):
-
-    """ Проверка подключения к PostgreSQL, существования таблиц """   
-    try:        
-        with engine.connect() as conn:           
-            #Проверка существования таблицы с  данными     
-            exist_table =  conn.execute(text(f"""select exists(
-                                            select * 
-                                            from information_schema.tables
-                                            where table_schema = '{schema}' 
-                                            and table_name = '{table_name}')""")).first()[0]                
-            conn.commit()                          
-            if exist_table is False:
-                logging('error_exist_table', engine, f'Таблицы {schema}.{table_name} не существует.')
-                print(f"\nТаблицы c данными {schema}.{table_name} в PostgreSQL не существует.\n")                          
-            return exist_table
-    except:       
-        print(f'ОШИБКА соединения c PostgreSQL: \n {sys.exc_info()}\n')
- 
+    """
+    Проверка существования витрины в PostgreSQL и данных в ней.
+    """            
+    with engine.connect() as conn:        
+        exist_table =  conn.execute(text(f"""select exists(
+                                        select * 
+                                        from information_schema.tables
+                                        where table_schema = '{schema}' 
+                                        and table_name = '{table_name}')""")).first()[0]                
+        if exist_table:
+            data = conn.execute(text(f"""select * from {schema}.{table_name};""")).first()   
+            if data != None:
+                return True  
+            else:
+                logging('error_exist_table', engine,  f'ОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные.', '') 
+                print(f'ОШИБКА. В витрине {schema}.{table_name} отстутсвуют данные.\n')  
+        conn.close()                             
+        if not exist_table:
+            logging('error_exist_table', engine, f'Таблицы {schema}.{table_name} не существует.')
+            print(f"Витрины {schema}.{table_name} в PostgreSQL не существует.\n")              
+        
 
 def extract_PostgreSQL(engine, schema, table_name):
+    """
+    Извлечение данных из PostgreSQL.
+    """      
+    df = pd.read_sql_query(f'select * from {schema}.{table_name}', con=engine)                     
+    logging('extract_PostgreSQL', engine,  f'Извлечение данных из витрины {schema}.{table_name} PostgreSQL прошло успешно.', '')
+    print(f'Извлечение данных из витрины {schema}.{table_name} PostgreSQL прошло успешно.\n')                
+    return df          
+    
+ 
+def upload_to_csv(engine, df, table_name, file_to_open):  
+    """
+    Загрузка данных в csv файл.
+    """            
+    df.to_csv(file_to_open, sep=';', encoding='utf-8', index=False)          
+    logging('upload_to_csv', engine, f'Данные загружены в файл {table_name}.csv.')
+    print(f'Данные загружены в файл {table_name}.csv.\n')         
+    
 
-    """ Извлечение данных из PostgreSQL. """   
-    try:
-        df = pd.read_sql_query(f'select * from {schema}.{table_name}', con=engine) 
-        if not df.empty:                 
-            logging('extract_PostgreSQL', engine,  f'Извлечение данных из таблицы {schema}.{table_name} PostgreSQL прошло успешно.', '')
-            print(f'Извлечение данных из таблицы {schema}.{table_name} PostgreSQL прошло успешно.')                
-            return df
-        else:
-            logging('error_extract_PostgreSQL', engine,  f'ОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные.', '') 
-            print(f'ОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные.')              
-    except:
-        logging('error_extract_PostgreSQL', engine,  f'ОШИБКА извлечения данных из таблицы {schema}.{table_name} PostgreSQL',\
-                str(sys.exc_info()).replace("'", '')) 
-        print(f'Ошибка извлечения данных из таблицы {schema}.{table_name} PostgreSQL: \n {sys.exc_info()}')
-  
-
-def upload_to_csv(engine, df, table_name):
-     
-    """ Загрузка данных в csv файл. """   
-    try:
-        df.to_csv(file_to_open, sep=';', encoding='utf-8', index=False)          
-        logging('upload_to_csv', engine, f'Данные успешно загружены в файл {table_name}.csv.')
-        print(f'Данные успешно загружены в файл {table_name}.csv.\n')         
-    except:
-        logging('error_upload_to_csv', engine, f'ОШИБКА загрузки данных в файл {table_name}.csv.',\
-                str(sys.exc_info()).replace("'", '')) 
-        print(f"Ошибка загрузки данных в файл {table_name}.csv.", str(sys.exc_info()).replace("'", '') ) 
-
-
-def upload_PostgreSQL(engine, schema, table_name, copy_table_name):
-
-    """ Загрузка данных из .csv в PostgreSQL. """        
-    try:
-        df = pd.read_csv(file_to_open, delimiter=';', encoding='utf-8')          
-        if not df.empty:    
-            with engine.connect() as conn:       
-                conn.execute(text(f"""drop table if exists {schema}.{copy_table_name};"""))
-                conn.execute(text(f"""create table if not exists {schema}.{copy_table_name} as table {schema}.{table_name} with no data;"""))
-                conn.commit()     
-            df.to_sql(copy_table_name, engine, if_exists='append', schema=schema, index=False)   
-            logging('upload_PostgreSQL', engine, f'Данные в таблицу {schema}{copy_table_name} успешно загружены.')
-            print(f'Данные в таблицу {schema}.{copy_table_name} загружены.\n') 
-        else:
-            logging('error_upload_PostgreSQL', engine,  f'ОШИБКА. В файле {file_to_open}.csv отстутсвуют данные', '') 
-            print(f'ОШИБКА. В файле {file_to_open}.csv отстутсвуют данные.')  
-    except:       
-        logging('error_upload_PostgreSQL', engine, f'ОШИБКА загрузки данных в таблицу {schema}.{copy_table_name}',\
-                str(sys.exc_info()).replace("'", '')) 
-        print(f"Ошибка загрузки данных в таблицу {schema}.{copy_table_name}.", str(sys.exc_info()).replace("'", '') ) 
-
+def upload_PostgreSQL(engine, schema, table_name, copy_table_name, file_to_open):
+    """
+    Загрузка данных из .csv в PostgreSQL.
+    """    
+    df = pd.read_csv(file_to_open, delimiter=';', encoding='utf-8')     
+    with engine.connect() as conn:       
+        conn.execute(text(f"""drop table if exists {schema}.{copy_table_name};"""))
+        conn.execute(text(f"""create table if not exists {schema}.{copy_table_name} as table {schema}.{table_name} with no data;"""))
+        conn.commit()     
+        conn.close() 
+    df.to_sql(copy_table_name, engine, if_exists='append', schema=schema, index=False)   
+    logging('upload_PostgreSQL', engine, f'Данные в таблицу {schema}{copy_table_name} успешно загружены.')
+    print(f'Данные в таблицу {schema}.{copy_table_name} загружены.\n') 
+    
 
 def main():
-
-    start_time = time.time()       
-    engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
-    exis_table = exist_table(engine)        
-    if exis_table is True: 
-        choice = 0
-        while choice != 1 or choice != 2: 
-            try:    
-                choice = int(input(f'\nЕсли вы хотите загрузить данные в файл {table_name}.csv - введите цифру 1.'
-                        f'\nЕсли необходимо загрузить данные в таблицу {copy_table_name} в PostreSQL - введите цифру 2.\n'
-                        f'Для выхода введите цифру 3: \n'))                   
-            except:  
-                pass             
-            if choice == 1:
-                df = extract_PostgreSQL(engine)                                                         
-                if df is not None:   
-                    upload_to_csv(engine, df)                    
-                    break 
-                else:             
-                    break                                     
-            elif choice == 2:
-                upload_PostgreSQL(engine)                
-                break  
-            elif choice == 3:
-                print('Выход из программы.')               
-                break                                                                             
-            else:
-                print("ВВедите, пожалуйста, цифру 1, 2 или 3 для выхода.") 
-    
-    print("--- %s seconds ---" % (time.time() - start_time))
-
+    """
+    Запуск программы.
+    """
+    start_time = time.time()    
+    # Название таблиц, схем в PostgreSQL.
+    schema = 'dm'
+    table_name = 'dm_f101_round_f'
+    copy_table_name = 'dm_f101_round_f_2'
+    # Путь к файлу.
+    file_to_open = Path("Project_Data-Engineering/Task_1_3/data")/ f"{table_name}.csv"    
+    keyboard_input = 0
+    while keyboard_input not in [1,2,3]: 
+        try:
+            keyboard_input = int(input(
+            f'Введите 1 - загрузка данных в файл {table_name}.csv.\n'
+            f'Введите 2 - загрузка данных в таблицу {copy_table_name} в PostreSQL.\n'
+            f'Введите 3  - выход: \n'))
+        except:
+            pass        
+        if keyboard_input ==3:
+            print('Конец программы.')
+            break 
+        if keyboard_input not in [1,2,3]:
+            print('\nНеверный ввод.\n')
+            continue
+        engine = connect_postgreSQL() 
+        if engine:
+            сreate_table_logging(engine)                   
+            if exist_table(engine, schema, table_name):                                  
+                if keyboard_input ==1:                
+                    df = extract_PostgreSQL(engine, schema, table_name)
+                    upload_to_csv(engine, df, table_name, file_to_open)
+                    print("--- %s seconds ---" % (time.time() - start_time)) 
+                if keyboard_input ==2:    
+                    upload_PostgreSQL(engine, schema, table_name, copy_table_name, file_to_open)
+                    print("--- %s seconds ---" % (time.time() - start_time)) 
+                  
 
 if __name__ == '__main__':
     main()
--- Project_Data-Engineering/Task_1_4/main.py
+++ Project_Data-Engineering/Task_1_4/main.py
@@ -5,114 +5,14 @@ from os import getenv
 from dotenv import load_dotenv, find_dotenv #(python-dotenv)
 import sys
 from pathlib import Path
+import datetime
 load_dotenv(find_dotenv()) # Для использования переменных окружения.
 
 
-def сreate_table(engine):
+def connect_postgreSQL():
 
-    with engine.connect() as conn:   
-        #Создание таблицы для логов.
-        conn.execute(text(f"""create table if not exists logs.extract_function_logs(
-                                action_date timestamp not null default now(),
-                                status varchar(40),
-                                description text,
-                                error text);"""))         
-        conn.commit()  
+    """Провека подключения к PosgreSQL"""
 
-
-def logging(status, engine, description='', error=''):
-
-    """ Загрузка данных в таблицу логов. """   
-
-    with engine.connect() as conn:             
-        conn.execute(text(f"""insert into logs.extract_function_logs (status, description, error)
-                          values ('{status}', '{description}', '{error}');"""))
-        conn.commit()         
-
-
-def exist_object(engine, schema, table_name, schema2, function_name):
-
-    """ Проверка подключения к PostgreSQL, существования таблицы с данными, функции. """   
-    try: 
-        #Проверка существования таблицы с данными.       
-        with engine.connect() as conn:            
-            exist_table =  conn.execute(text(f"""select exists
-                                            (select * 
-                                            from information_schema.tables
-                                            where table_schema = '{schema}' 
-                                            and table_name = '{table_name}')""")).first()[0] 
-            #Проверка наличия данных в таблице.
-            data = conn.execute(text(f"""select * from ds.ft_posting_f;""")).first()                              
-            #Проверка существования функции.   
-            exist_function =  conn.execute(text(f"""select exists
-                                            (select * 
-                                            from pg_proc 
-                                            where proname = '{function_name}')""")).first()[0]          
-            conn.commit()                          
-            if exist_table is False:
-                logging('error_exist_object', engine, f'Таблицы {schema}.{table_name} не существует.')
-                print(f"\nТаблицы c данными {schema}.{table_name} в PostgreSQL не существует.\n")                          
-           
-            elif exist_function is False:
-                logging('error_exist_object', engine, f'Функции {schema2}.{function_name} не существует.')
-                print(f"\nФункции {schema2}.{function_name} не существует.\n")
-
-            elif data == None:
-                logging('error_exist_object', engine, f'Нет данных в таблице  {schema}.{table_name}.')
-                print(f'\nНет данных в таблице  {schema}.{table_name}.\n')  
-            
-            return exist_table, exist_function, data
-    except:       
-        print(f'ОШИБКА соединения c PostgreSQL: \n {sys.exc_info()}\n')
-
- 
-def start_function(engine, schema, table_name, schema2, function_name, date):
-
-    """ Функция в PostgreSQL принимает дату и возвращает эту дату и информацию о максимальной и минимальной сумме проводки 
-        по кредиту и по дебету за переданную дату """   
-    try:
-        #Создание датафрейма и извлечение данных с использованием функции в PostgreSQL.
-        df = pd.read_sql_query(f"""select * from {schema2}.{function_name}('{date}')""", con=engine)
-        #Проверка, что в датафрейме есть данные.          
-        if not df.empty:                 
-            logging('start_function', engine,  f'ФУнкция {schema2}.{function_name} отработала успешно.', '')
-            print(f'\nФУнкция {schema2}.{function_name} отработала успешно.\n')                
-            return df
-        else:
-            logging('error_start_function', engine,  f'ОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные за указанную дату.', '') 
-            print(f'\nОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные за указанную дату.\n')              
-    except:
-        logging('error_start_function', engine,  f'ОШИБКА извлечения данных из таблицы {schema}.{table_name} PostgreSQL',\
-                 str(sys.exc_info()).replace("'", '')) 
-        print(f'Ошибка работы функции {schema2}.{function_name} PostgreSQL: \n {sys.exc_info()}')        
-
-
-def upload_to_csv(engine, df, date, function_name):
-     
-    """ Загрузка данных в csv файл. """   
-    try:          
-        # Путь к файлу .csv и его название
-        file_to_open = Path("Project_Data-Engineering/Task_1_4/data")/ f"{date}_{function_name}.csv" 
-        #Запись данных в файл .csv
-        df.to_csv(file_to_open, sep=';', encoding='utf-8', index=False)          
-        logging('upload_to_csv', engine, f'Данные успешно загружены в файл {function_name}.csv.')
-        print(f'Данные загружены в файл {function_name}.csv.\n')         
-    except:
-        logging('error_upload_to_csv', engine, f'ОШИБКА загрузки данных в файл {function_name}.csv.',\
-                str(sys.exc_info()).replace("'", '')) 
-        print(f"Ошибка загрузки данных в файл {function_name}.csv.", str(sys.exc_info()).replace("'", '') ) 
-
-
-def main(date):
-
-    start_time = time.time()    
-    # Название таблиц, схем в PostgreSQL.
-    schema = 'ds'
-    table_name = 'ft_posting_f'
-    schema2 = 'dm'
-    function_name = 'func_ds_ft_posting_f'
-
-    #Переменные окружения для PostgreSQL:
     DB_NAME = getenv("DB_NAME")
     DB_HOST=getenv("DB_HOST")
     DB_PORT=getenv("DB_PORT")
@@ -122,17 +22,143 @@ def main(date):
     engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
     try:
-        сreate_table(engine)        
-        exis_table, exis_function, data_empty = exist_object(engine, schema, table_name, schema2, function_name)         
-        if exis_table is True and exis_function is True and data_empty != None: 
-            df = start_function(engine, schema, table_name, schema2, function_name, date)
-            if df is not None:         
-                upload_to_csv(engine, df, date, function_name)    
-    except:
-        pass
-        
-    print("--- %s seconds ---" % (time.time() - start_time))
+        engine.connect()
+        print("\nПодключение к PostgreSQL успешно.\n") 
+        return engine  
+    except:        
+        print(f'ОШИБКА соединения c PostgreSQL: \n {sys.exc_info()}\n')
+
+
+def сreate_table_logging(engine):
+    
+    """ Создание таблицы логов. """
+
+    with engine.connect() as conn:          
+        conn.execute(text(f"""create table if not exists logs.extract_function_logs(
+                                action_date timestamp not null default now(),
+                                status varchar(40),
+                                description text,
+                                error text);"""))         
+        conn.commit() 
+        conn.close() 
+
+
+def logging(status, engine, description='', error=''):
+
+    """ Загрузка данных в таблицу логов. """   
+
+    with engine.connect() as conn:             
+        conn.execute(text(f"""insert into logs.extract_function_logs (status, description, error)
+                          values ('{status}', '{description}', '{error}');"""))
+        conn.commit()
+        conn.close()         
+
+
+def exist_table(engine, schema, table_name):
+
+    """ Проверка существования таблицы в PostgreSQL и данных в ней. """   
+    
+    #Проверка существования таблицы с данными.       
+    with engine.connect() as conn:            
+        exist_table =  conn.execute(text(f"""select exists
+                                        (select * 
+                                        from information_schema.tables
+                                        where table_schema = '{schema}' 
+                                        and table_name = '{table_name}')""")).first()[0] 
+        #Проверка наличия данных в таблице.
+        if exist_table:
+            data = conn.execute(text(f"""select * from {schema}.{table_name};""")).first()   
+            if data != None:
+                return True
+            else:
+                logging('error_exist_object', engine, f'Нет данных в таблице  {schema}.{table_name}.')
+                print(f'Нет данных в таблице  {schema}.{table_name}.\n')                           
+        conn.close()    
+        if not exist_table:
+            logging('error_exist_object', engine, f'Таблицы {schema}.{table_name} не существует.')
+            print(f"Таблицы c данными {schema}.{table_name} в PostgreSQL не существует.\n")         
+         
+      
+def exist_function(engine, schema2, function_name):
+
+    """  Проверка cуществования функции в PostgreSQL. """   
+    
+    #Проверка существования таблицы с данными.       
+    with engine.connect() as conn:           
+        exist_function =  conn.execute(text(f"""select exists
+                                        (select * 
+                                        from pg_proc 
+                                        where proname = '{function_name}')""")).first()[0]          
+        conn.close() 
+        if exist_function:
+            return True            
+        if not exist_function:
+            logging('error_exist_object', engine, f'Функции {schema2}.{function_name} не существует.')
+            print(f"\nФункции {schema2}.{function_name} не существует.\n")      
+
+
+def start_function(engine, schema, table_name, schema2, function_name, date):
+
+    """ Функция в PostgreSQL принимает дату и возвращает эту дату и информацию о максимальной и минимальной сумме проводки 
+        по кредиту и по дебету за переданную дату """   
+  
+        #Создание датафрейма и извлечение данных с использованием функции в PostgreSQL.
+    df = pd.read_sql_query(f"""select * from {schema2}.{function_name}('{date}')""", con=engine)
+    #Проверка, что в датафрейме есть данные.          
+    if not df.empty:                 
+        logging('start_function', engine,  f'ФУнкция {schema2}.{function_name} отработала успешно.', '')
+        print(f'\nФУнкция {schema2}.{function_name} отработала успешно.\n')                
+        return df
+    else:
+        logging('error_start_function', engine,  f'ОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные за указанную дату.', '') 
+        print(f'\nОШИБКА. В таблице {schema}.{table_name} отстутсвуют данные за указанную дату.\n') 
+
+
+def upload_to_csv(engine, df, date, function_name):
+     
+    """ Загрузка данных в csv файл. """   
+            
+    # Путь к файлу .csv и его название
+    file_to_open = Path("Project_Data-Engineering/Task_1_4/data")/ f"{date}_{function_name}.csv" 
+    #Запись данных в файл .csv
+    df.to_csv(file_to_open, sep=';', encoding='utf-8', index=False)          
+    logging('upload_to_csv', engine, f'Данные успешно загружены в файл {function_name}.csv.')
+    print(f'Данные загружены в файл {function_name}.csv.\n')      
+
+
+def date_validation():
+
+    """Проверка корректности даты"""
+
+    date = input(f'Введите дату проводки в формате 2018-01-01: ')
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')       
+        return date
+    except ValueError:
+        print("Некорректный формат даты.")
+
+
+def main():
+
+    start_time = time.time()    
+    # Название таблиц, схем в PostgreSQL.
+    schema = 'ds'
+    table_name = 'ft_posting_f'
+    schema2 = 'dm'
+    function_name = 'func_ds_ft_posting_f'
+
+    engine = connect_postgreSQL()   
+
+    if engine:                   
+        if exist_table(engine, schema, table_name):  
+            if exist_function(engine, schema2, function_name):
+                date = date_validation()    
+                if date:       
+                    df = start_function(engine, schema, table_name, schema2, function_name, date)
+                    if df is not None:         
+                        upload_to_csv(engine, df, date, function_name) 
+                        print("--- %s seconds ---" % (time.time() - start_time))     
 
 
 if __name__ == '__main__':
-    main( '2018-01-09')
+    main()
